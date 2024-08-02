6d125d58d4157d7b4625d6b0813599ab7cfa8355
Muhammad Burhannudin Hamzah <muhammad_hamzah_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 14:14:08 +0700

    Add files via upload

---
+++ TES.java
@@ -0,0 +1,6 @@
+public class TES {
+    public static void main(String[] args) {
+        System.out.println("Nama saya Burhan dari kelas X RPL 7 saya berasal dari Tulunganggung");
+        String a = "1";   
+    }
+}
