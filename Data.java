2025d2c6853031e72f06d41584faed64ecbbdf8c
Muhammad Burhannudin Hamzah <muhammad_hamzah_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 11:56:10 +0700

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,14 @@
+public class Data {
+public static void main(String[] args) {
+    //tipe_data nama_var = value/isi var;
+    String nama = "Burhan";
+    int usia = 16;
+    String alamat = "JL. Danau Ranau Sawojajar";
+    double kelas = 10.7;
+
+    System.out.println("nama saya " + nama);
+    System.out.println("usia :" + usia + " tahun ");
+    System.out.println("Alamat : " + alamat);
+    System.out.println("kelas : " + kelas);
+}
+}
