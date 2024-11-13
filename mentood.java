b167ee1caf59d47e554405d490611f67ea8e9f32
Muhammad Burhannudin Hamzah <muhammad_hamzah_33rpl@student.smktelkom-mlg.sch.id>
2024-11-13 19:58:36 +0700

    Add files via upload

---
+++ mentood.java
@@ -0,0 +1,32 @@
+static void jumlah(int a, int b) {
+    System.out.println("5+8 jumlah nya adalah : "+(a+b));
+    
+}
+
+static void kurang(int a, int b) {
+    System.out.println("5-8 jumlah nya adalah : "+(a-b));
+    
+}
+static void kali(int a, int b) {
+    System.out.println("5x8 jumlah nya adalah : "+(a*b));
+    
+}
+static void bagi(double a, double b) {
+    System.out.println("5-8 jumlah nya adalah : "+(a/b));
+    
+}
+public static void main(String[] args) {
+    
+    int a= 5;
+    int b= 8;
+    
+    
+
+    jumlah(a, b);
+    kurang(a, b);
+    kali(a, b);
+    bagi(a, b);
+}
+
+
+}
